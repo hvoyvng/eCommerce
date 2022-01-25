8384231f68670f9a3552a5c213a5f0e71df1bf48
hvoyvng <jyu0628@gmail.com>
2022-01-25 10:57:12 -0800

    fixing seed data

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const { Product, Category, Tag, ProductTag } = require('./models')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(require('./routes'))
+
+async function init() {
+    await require('./config/connection.js').sync()
+    app.listen(process.env.PORT || 3000)
+}
+
+init()
